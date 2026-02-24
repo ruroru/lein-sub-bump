b3faabcfd71bc6f950ed3d468b551c2385fe1770
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-24 00:06:56 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/lein-sub-bump "1.0.0"
+(defproject org.clojars.jj/lein-sub-bump "1.0.1-SNAPSHOT"
   :description "Leiningen plugin to synchronize subproject versions"
   :url "https://github.com/ruroru/lein-sub-bump"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
